f0309b17bd8628077b882a63f77f80122ff5bfd7
fRen4505 <ren05.pj@outlook.com>
2024-11-11 13:30:54 -0300

    Base

---
+++ src/test/Projec/ProjecApplicationTests.java
@@ -0,0 +1,13 @@
+package Java.projetoMaromo.Projec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
